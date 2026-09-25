8e36d887e1e7a87a5f016efeaf9d98ebcdbb80dd
Артур <masyavitch@gmail.com>
2026-09-25 08:41:07 +0300

    Fix C++ formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <string>
 
-std::string getGreeting() {
-    return "Hello from C++ in Docker! 🐳";
+std::string getGreeting()
+{
+  return "Hello from C++ in Docker! 🐳";
 }
 
-int main() {
-    std::cout << getGreeting() << std::endl;
-    return 0;
+int main()
+{
+  std::cout << getGreeting() << std::endl;
+  return 0;
 }
